a7ccaddffef9eadd7681e73a5fb35691ff7b3d6d
cputnam-a11y <chrlsvryptnm@gmail.com>
2025-05-05 17:21:26 -0500

    replace ThreadLocal local in net.fabricmc.fabric.mixin.item.BrewingStandBlockEntityMixin with @Share

--- fabric-item-api-v1/src/main/java/net/fabricmc/fabric/mixin/item/BrewingStandBlockEntityMixin.java
+++ fabric-item-api-v1/src/main/java/net/fabricmc/fabric/mixin/item/BrewingStandBlockEntityMixin.java
@@ -17,8 +17,9 @@
 package net.fabricmc.fabric.mixin.item;
 
 import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -33,21 +34,17 @@ import net.minecraft.world.World;
 
 @Mixin(BrewingStandBlockEntity.class)
 public class BrewingStandBlockEntityMixin {
-	@Unique
-	private static final ThreadLocal<ItemStack> REMAINDER_STACK = new ThreadLocal<>();
 
 	@Inject(method = "craft", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;decrement(I)V", shift = At.Shift.AFTER))
-	private static void captureItemStack(World world, BlockPos pos, DefaultedList<ItemStack> slots, CallbackInfo ci, @Local ItemStack itemStack) {
-		REMAINDER_STACK.set(itemStack.getRecipeRemainder());
+	private static void captureItemStack(World world, BlockPos pos, DefaultedList<ItemStack> slots, CallbackInfo ci, @Local ItemStack itemStack, @Share("remainder") LocalRef<ItemStack> remainderRef) {
+		remainderRef.set(itemStack.getRecipeRemainder());
 	}
 
 	/**
 	 * Wrap the {@link Item#getRecipeRemainder} call to replace the old remainder with the new one.
 	 */
 	@Redirect(method = "craft", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item;getRecipeRemainder()Lnet/minecraft/item/ItemStack;"))
-	private static ItemStack createStackRecipeRemainder(Item item) {
-		ItemStack remainder = REMAINDER_STACK.get();
-		REMAINDER_STACK.remove();
-		return remainder;
+	private static ItemStack createStackRecipeRemainder(Item item, @Share("remainder") LocalRef<ItemStack> remainderRef) {
+		return remainderRef.get();
 	}
 }
